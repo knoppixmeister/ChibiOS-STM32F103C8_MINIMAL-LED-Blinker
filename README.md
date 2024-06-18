243c4b73890f4deed8419cb3af1cacdfbb27a13c
knoppixmeister <knoppixmeister@gmail.com>
2024-06-19 01:38:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# BluePill-ChibiOS
+Green LED blinking app for STM32F103C8 (Blue pill) board with modern code on stable ChibiOS v.21.11.x
+
+How to:
+```
+git clone https://github.com/knoppixmeister/ChibiOS-STM32F103C8_MINIMAL-LED-Blinker.git
+cd ChibiOS-STM32F103C8_MINIMAL-LED-Blinker
+git submodule update --init
+make
+```
+
+Code used: https://github.com/ChibiOS/ChibiOS/tree/master/demos/STM32/RT-STM32F103-MAPLEMINI
+Inspired by code: https://github.com/ddB0515/BluePill-ChibiOS
---
+++ main.c
@@ -0,0 +1,41 @@
+#include "hal.h"
+#include "ch.h"
+
+/*
+ * Blinker thread, times are in milliseconds.
+*/
+static THD_WORKING_AREA(waThread1, 128);
+static __attribute__((noreturn)) THD_FUNCTION(Thread1, arg) {
+	(void) arg;
+
+	chRegSetThreadName("blinker");
+
+	while (true) {
+		// systime_t time = serusbcfg.usbp->state == USB_ACTIVE ? 250 : 500;
+
+		palClearPad(GPIOC, GPIOC_LED);
+		chThdSleepMilliseconds(1000);
+
+		palSetPad(GPIOC, GPIOC_LED);
+		chThdSleepMilliseconds(1000);
+	}
+}
+
+int main(void) {
+	halInit();
+	chSysInit();
+
+	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+	while(true) {
+		// palSetPad(GPIOC, GPIOC_LED);
+		// chThdSleepMilliseconds(1000);
+
+		// palSetPad(GPIOC, 13);
+
+		// palClearPad(GPIOC, GPIOC_LED3);
+		chThdSleepMilliseconds(1000);
+	}
+
+  return 0;
+}
